5cf26fe477028efae30e7e54e39dac12a821b17e
michaelchanwcl <kei12457>
2021-12-20 11:08:18 +0800

    Add docker support

--- nuxeo/addons/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/blob/s3/S3BlobStoreConfiguration.java
+++ nuxeo/addons/nuxeo-core-binarymanager-cloud/nuxeo-core-binarymanager-s3/src/main/java/org/nuxeo/ecm/blob/s3/S3BlobStoreConfiguration.java
@@ -20,7 +20,9 @@ package org.nuxeo.ecm.blob.s3;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
-import static org.nuxeo.ecm.blob.s3.S3BlobStoreConfiguration.DISABLE_PROXY_PROPERTY;
+import static org.nuxeo.common.concurrent.ThreadFactories.newThreadFactory;
+import static org.nuxeo.ecm.core.blob.BlobProviderDescriptor.RECORD;
+import static org.nuxeo.ecm.core.model.Session.PROP_RETENTION_COMPLIANCE_MODE_ENABLED;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -35,6 +37,7 @@ import java.time.Duration;
 import java.util.Date;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.Executors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -56,13 +59,12 @@ import com.amazonaws.services.s3.AmazonS3Builder;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.AmazonS3EncryptionClientBuilder;
 import com.amazonaws.services.s3.model.CryptoConfiguration;
-import com.amazonaws.services.s3.model.DefaultRetention;
 import com.amazonaws.services.s3.model.EncryptionMaterials;
 import com.amazonaws.services.s3.model.GetObjectLockConfigurationRequest;
 import com.amazonaws.services.s3.model.GetObjectLockConfigurationResult;
 import com.amazonaws.services.s3.model.ObjectLockConfiguration;
+import com.amazonaws.services.s3.model.ObjectLockEnabled;
 import com.amazonaws.services.s3.model.ObjectLockRetentionMode;
-import com.amazonaws.services.s3.model.ObjectLockRule;
 import com.amazonaws.services.s3.model.StaticEncryptionMaterialsProvider;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.TransferManagerBuilder;
@@ -197,14 +199,26 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
     public static final long MINIMUM_UPLOAD_PART_SIZE_DEFAULT = 5L * 1024 * 1024; // AWS SDK default = 5 MB
 
     /**
+     * The Framework property to define the transfer manager thread pool size.
+     *
+     * @since 10.10-HF57
+     */
+    public static final String TRANSFER_MANAGER_THREAD_POOL_SIZE_PROPERTY = "nuxeo.s3storage.transfer.manager.thread.pool.size";
+
+    /**
+     * The default value for the transfer manager thread pool size.
+     *
+     * @since 10.10-HF57
+     */
+    public static final int TRANSFER_MANAGER_THREAD_POOL_SIZE_DEFAULT = 10;
+
+    /**
      * Framework property to disable usage of the proxy environment variables ({@code nuxeo.http.proxy.*}) for the
      * connection to the S3 endpoint.
      *
      * @since 11.1
      */
     public static final String DISABLE_PROXY_PROPERTY = "nuxeo.s3.proxy.disabled";
-
-    public static final ObjectLockRetentionMode DEFAULT_RETENTION_MODE = ObjectLockRetentionMode.GOVERNANCE;
 
     public final CloudFrontConfiguration cloudFront;
 
@@ -225,11 +239,11 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
     public final boolean metadataAddUsername;
 
     /**
-     * The retention mode with which the bucket is configured.
+     * Is Object Lock feature enabled at s3 level.
      *
-     * @since 11.4
+     * @since 2021.13
      */
-    public final ObjectLockRetentionMode bucketRetentionMode;
+    public final boolean s3RetentionEnabled;
 
     /**
      * The retention mode to use when setting the retention on an object.
@@ -278,8 +292,14 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
         amazonS3 = getAmazonS3(s3Builder);
 
         metadataAddUsername = getBooleanProperty(METADATA_ADD_USERNAME_PROPERTY);
-        bucketRetentionMode = computeBucketRetentionMode();
-        retentionMode = bucketRetentionMode == null ? DEFAULT_RETENTION_MODE : bucketRetentionMode;
+
+        s3RetentionEnabled = isS3RetentionEnabled();
+        retentionMode = Framework.isBooleanPropertyTrue(PROP_RETENTION_COMPLIANCE_MODE_ENABLED)
+                ? ObjectLockRetentionMode.COMPLIANCE
+                : ObjectLockRetentionMode.GOVERNANCE;
+        if (!s3RetentionEnabled && Boolean.parseBoolean(properties.get(RECORD))) {
+            log.warn("Blob provider is configured for records but retention is not enabled on s3 bucket {}", bucketName);
+        }
 
         transferManager = createTransferManager();
 
@@ -510,7 +530,7 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
 
     protected TransferManager createTransferManager() {
         // when the bucket has Object Lock active, uploads need to provide an MD5
-        boolean alwaysCalculateMultipartMd5 = bucketRetentionMode != null;
+        boolean alwaysCalculateMultipartMd5 = s3RetentionEnabled;
         return TransferManagerBuilder.standard()
                                      .withS3Client(amazonS3)
                                      .withMinimumUploadPartSize(Long.valueOf(getLongProperty(
@@ -521,31 +541,33 @@ public class S3BlobStoreConfiguration extends CloudBlobStoreConfiguration {
                                              MULTIPART_COPY_THRESHOLD_PROPERTY, MULTIPART_COPY_THRESHOLD_DEFAULT)))
                                      .withMultipartCopyPartSize(Long.valueOf(getMultipartCopyPartSize()))
                                      .withAlwaysCalculateMultipartMd5(alwaysCalculateMultipartMd5)
+                                     .withExecutorFactory(() -> Executors.newFixedThreadPool(
+                                             (int) getLongProperty(TRANSFER_MANAGER_THREAD_POOL_SIZE_PROPERTY,
+                                                     TRANSFER_MANAGER_THREAD_POOL_SIZE_DEFAULT),
+                                             newThreadFactory("s3-transfer-manager-worker")))
                                      .build();
     }
 
     /** @deprecated since 11.4, unused */
     @Deprecated
     protected ObjectLockRetentionMode getRetentionMode() {
-        ObjectLockRetentionMode bucketRetentionMode = computeBucketRetentionMode();
-        return bucketRetentionMode == null ? DEFAULT_RETENTION_MODE : bucketRetentionMode;
+        return retentionMode;
     }
 
-    protected ObjectLockRetentionMode computeBucketRetentionMode() {
+    protected boolean isS3RetentionEnabled() {
         GetObjectLockConfigurationRequest request = new GetObjectLockConfigurationRequest().withBucketName(bucketName);
         GetObjectLockConfigurationResult result;
         try {
             result = amazonS3.getObjectLockConfiguration(request);
         } catch (AmazonServiceException e) {
             log.debug("Failed to get ObjectLockConfiguration for bucket: {}", bucketName, e);
-            return null;
+            return false;
         }
-        return Optional.ofNullable(result.getObjectLockConfiguration())
-                       .map(ObjectLockConfiguration::getRule)
-                       .map(ObjectLockRule::getDefaultRetention)
-                       .map(DefaultRetention::getMode)
-                       .map(ObjectLockRetentionMode::valueOf)
-                       .orElse(null);
+        ObjectLockConfiguration olc = result.getObjectLockConfiguration();
+        if (olc != null) {
+            return ObjectLockEnabled.ENABLED.toString().equals(olc.getObjectLockEnabled());
+        }
+        return false;
     }
 
     /**
